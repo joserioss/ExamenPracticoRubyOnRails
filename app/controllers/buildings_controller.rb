10616bb9e07c0235dfbebd3c7c918b46d108b7c2
jriosMELI <jose.rios@mercadolibre.cl>
2021-12-19 12:30:10 -0300

    index building

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,4 +1,17 @@
 class BuildingsController < ApplicationController
+  def index
+    @buildings = Building.all
+  end
+
+  def new
+  end
+
   def show
+  end
+
+  def create
+  end
+
+  def update
   end
 end
