7d58f6ed34bbdf73007bdab8ed3e4f5a240935af
jriosMELI <jose.rios@mercadolibre.cl>
2021-12-19 12:18:50 -0300

    Controller Apartments

---
+++ app/controllers/apartments_controller.rb
@@ -0,0 +1,16 @@
+class ApartmentsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'apartments/index'
+  get 'apartments/new'
+  get 'apartments/show'
+  get 'apartments/create'
+  get 'apartments/update'
   resources :buildings
   resources :apartments
 end
