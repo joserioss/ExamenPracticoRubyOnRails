208685a37ad1c91076828caabe86f300a2983272
jriosMELI <jose.rios@mercadolibre.cl>
2021-12-19 11:22:05 -0300

    Incorporacion validaciones en modelos

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,3 +1,19 @@
 class Apartment < ApplicationRecord
   belongs_to :building
+
+  validates :number, presence: true, numericality:true
+
+  # esta callback se ejecutará después de crear un nuevo usuario
+  after_create :creacion_exitosa
+
+  # esta callback se ejecutará después de actualizar un usuario existente
+  after_update :actualizacion_exitosa
+
+  private
+  def creacion_exitosa
+    puts "Departamento creado con exito."
+  end
+  def actualizacion_exitosa
+    puts "Departamento actualizado con exito."
+  end
 end
--- app/models/building.rb
+++ app/models/building.rb
@@ -1,2 +1,23 @@
 class Building < ApplicationRecord
+    has_many :apartments, dependent: :destroy
+
+    validates :name, :address, :city, presence: true, length: { in: 2..50 }
+
+    def to_s
+      name
+    end
+
+    # esta callback se ejecutará después de crear un nuevo usuario
+    after_create :creacion_exitosa
+  
+    # esta callback se ejecutará después de actualizar un usuario existente
+    after_update :actualizacion_exitosa
+
+    private
+    def creacion_exitosa
+      puts "Edificio creado con exito."
+    end
+    def actualizacion_exitosa
+      puts "Edificio actualizado con exito."
+    end
 end
